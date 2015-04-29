cab59aa7c082182e2bb185ddfe33fce076c42961
Brandon <b93banks@gmail.com>
2015-04-29 10:23:07 -0400

    Update assembly.asm

--- assembly.asm
+++ assembly.asm
@@ -17,40 +17,40 @@ buzz: .asciiz "Buzz\n"
 space: .asciiz "\n"
 .text
 li $t1, 1
-while: 		bgt $t1, 100, done			#when i > 100 exit loop
+while: 		bgt $t1, 100, done		#when i > 100 exit loop
 body:
-ifmod3:   	rem $t2, $t1, 3			    # i%3
-        	bnez $t2, ifmod5		    # i%3=/=0 skip then mod 3
+ifmod3:   	rem $t2, $t1, 3			# i%3
+        	bnez $t2, ifmod5		# i%3=/=0 skip then mod 3
 
-ifboth:		rem $t2, $t1, 5			    # i%5
-        	bnez $t2, thenmod3		  	# i%5=/=0 skip then mod 5
+ifboth:		rem $t2, $t1, 5			# i%5
+        	bnez $t2, thenmod3		# i%5=/=0 skip then mod 5
 		
 thenboth:	li $v0, 4           		# to print a string
-		    la $a0, fizzbuzz     		# put address of null-terminted string in $a0
-		    syscall                 	# print the string
-		    j endwhile
+		la $a0, fizzbuzz     		# put address of null-terminted string in $a0
+		syscall                 	# print the string
+		j endwhile
 		
 thenmod3:	li $v0, 4           		# to print a string
-		    la $a0, fizz     		    # put address of null-terminted string in $a0
-		    syscall             		# print the string
-		    j endwhile	
+		la $a0, fizz     		# put address of null-terminted string in $a0
+		syscall             		# print the string
+		j endwhile	
 
-ifmod5:		rem $t2, $t1, 5			    # i%5
-		    bnez $t2, thennone		  	# i%5=/=0 skip then mod 5
+ifmod5:		rem $t2, $t1, 5		    	# i%5
+		bnez $t2, thennone	  	# i%5=/=0 skip then mod 5
 		
 thenmod5:	li $v0, 4           		# to print a string
-		    la $a0, buzz     		    # put address of null-terminted string in $a0
-		    syscall             		# print the string
-		    j endwhile
+		la $a0, buzz     		# put address of null-terminted string in $a0
+		syscall             		# print the string
+		j endwhile
 		
-thennone:	li $v0, 1         		  	# to print an integer
-		    move $a0, $t1       		# copy the output integer to $a0
-		    syscall             		# print the integer
-		    li $v0, 4           		# to print a string
-		    la $a0, space     		  	# put address of null-terminted string in $a0
-		    syscall             		# print the string
+thennone:	li $v0, 1         		# to print an integer
+		move $a0, $t1       		# copy the output integer to $a0
+		syscall             		# print the integer
+		li $v0, 4           		# to print a string
+		la $a0, space     		# put address of null-terminted string in $a0
+		syscall             		# print the string
 
-endwhile:	add $t1, $t1, 1			    # i++
-		    j while						# go back to test loop condition
+endwhile:	add $t1, $t1, 1			# i++
+		j while				# go back to test loop condition
 done:		li $v0, 10          		# to end program execution
-		    syscall             		# end program execution
+		syscall             		# end program execution
